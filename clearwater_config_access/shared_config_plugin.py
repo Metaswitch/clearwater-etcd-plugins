b98706446ffbddfa31c4e19c9fcbe928fd4043d0
Bennett Allen <bennett.allen@metaswitch.com>
2017-10-13 17:41:47 +0100

    Edits description of what needs to be done

--- clearwater_config_access/shared_config_plugin.py
+++ clearwater_config_access/shared_config_plugin.py
@@ -4,6 +4,8 @@ from configtype_plugin import ConfigType
 
 class SharedConfig(ConfigType):
     scripts = []  # this does have validation TODO
+    # make it run through all files in directory where shared config is
+    # this allows users to edit it too
     name = 'shared_config'
     uploadfile = 'upload_shared_config'
     help_info = 'shared_config is for  '  # TODO
