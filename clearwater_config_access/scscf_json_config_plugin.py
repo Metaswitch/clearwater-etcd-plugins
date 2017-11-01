c9fb85350c7114a0e5a6bd8a96b82d56551b9c00
Bennett Allen <bennett.allen@metaswitch.com>
2017-11-01 15:11:16 +0000

    Changes scscf help info

--- clearwater_config_access/scscf_json_config_plugin.py
+++ clearwater_config_access/scscf_json_config_plugin.py
@@ -19,7 +19,7 @@ class ScscfJson(ConfigType):
 
     # This help_info appears as user-visible help text in the usage statement
     # for cw-config.
-    help_info = ('''scscf_json - is s-cscf stores the configuration, capabilities,
+    help_info = ('''scscf_json - this stores the configuration, capabilities,
               relative weightings and priority of each S-CSCF.''')
     schema = '/usr/share/clearwater/clearwater-config-manager/scripts/config_validation/scscf_schema.json'
 
